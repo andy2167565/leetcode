2f1ac0c8a67fe2f0c136574dda4258299b0a67b0
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2020-12-18 17:44:31 +0800

    Create 9_palindrome-number.py

---
+++ 9_palindrome-number.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        
+        reversed_num = 0
+        while x > reversed_num:
+            r = x % 10
+            reversed_num = reversed_num * 10 + r 
+            x //= 10
+
+        return x == reversed_num or x == reversed_num//10
