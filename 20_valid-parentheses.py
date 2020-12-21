7dbbf2e40bb4dd93110adf7543d1c571059df585
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2020-12-21 15:25:49 +0800

    Create 20_valid-parentheses.py

---
+++ 20_valid-parentheses.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if len(set(s)) % 2 != 0:
+            return False
+        else:
+            bracket_list = [["(", ")"], ["[", "]"], ["{", "}"]]
+            stack = []
+            for i in list(s):
+                try:
+                    if stack:
+                        for j in bracket_list:
+                            if [stack[-1], i] == j:
+                                del stack[-1]
+                                raise Exception()
+                        stack.append(i)
+                    else:
+                        stack.append(i)
+                except Exception:
+                    continue
+            return not stack
