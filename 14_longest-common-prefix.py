da7b2fa1db24081a9ee2dbcbdbf68a5f24bad6bf
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2020-12-21 14:11:15 +0800

    Create 14_longest-common-prefix.py

---
+++ 14_longest-common-prefix.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        common_prefix = ""
+        if strs:
+            min_str = min(strs, key=len)
+            strs.remove(min_str)
+        else:
+            return common_prefix
+        
+        for index, char in enumerate(list(min_str)):
+            for word in strs:
+                if word[index] != char:
+                    return common_prefix
+            common_prefix += char
+        
+        return common_prefix
