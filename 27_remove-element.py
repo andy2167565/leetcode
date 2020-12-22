c6c6f239bf4a48b1eb9032fae075ed3cce6f8080
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2020-12-22 14:58:44 +0800

    Create 27_remove-element.py

---
+++ 27_remove-element.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        # <Two Pointers Solution>
+        if len(nums) == 0:
+            return 0
+        
+        remain_index = 0
+        for value in nums:
+            if value != val:
+                nums[remain_index] = value
+                remain_index += 1
+        
+        return remain_index
+        
+        # <Two Pointers with Rare Elements Solution>
+        if len(nums) == 0:
+            return 0
+        
+        current_index = 0
+        remain_index = len(nums)
+        while current_index < remain_index:
+            if nums[current_index] == val:
+                nums[current_index] = nums[remain_index-1]
+                remain_index -= 1
+            else:
+                current_index += 1
+        
+        return remain_index
