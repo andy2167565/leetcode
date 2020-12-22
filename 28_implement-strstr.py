fca4e7a4d01cb41114789b75baf2a83ea8a57020
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2020-12-22 16:50:55 +0800

    Create 28_implement-strstr.py

---
+++ 28_implement-strstr.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        # <Solution-1>
+        if not needle:
+            return 0
+        
+        hs_id = 0
+        hs_list = list(haystack)
+        nd_list = list(needle)
+        while hs_id < len(hs_list):
+            try:
+                if hs_list[hs_id:hs_id+len(nd_list)] != nd_list:
+                    raise Exception
+                return hs_id
+            except Exception:
+                hs_id += 1
+        return -1
+        
+        # <Solution-2>
+        for hs_id in range(len(haystack) - len(needle) + 1):
+            if all(haystack[hs_id+nd_id] == needle[nd_id] for nd_id in range(len(needle))):
+                return hs_id
+        return -1
+        
+        # <Solution-3>
+        try:
+            return haystack.index(needle)
+        except:
+            return -1
