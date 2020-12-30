a32366b4aef1bcd85939230330188a629e72d7c8
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2020-12-31 01:06:10 +0800

    Create 53_maximum-subarray.py

---
+++ 53_maximum-subarray.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # <Solution 1>
+        result = nums[0]
+        current_sum = 0
+        for num in nums:
+            # Remove negative prefix sum
+            if current_sum < 0:
+                current_sum = 0
+            current_sum += num
+            result = max(result, current_sum)
+        return result
+        
+        # <Solution 2>
+        result = nums[0]
+        current_sum = 0
+        for num in nums:
+            current_sum = max(num, current_sum + num)
+            result = max(result, current_sum)
+        return result
