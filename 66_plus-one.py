625d818960d39143f1fd28f631c1964691300d11
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2021-01-02 23:38:17 +0800

    Create 66_plus-one.py

---
+++ 66_plus-one.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        # <Solution 1>
+        num = int("".join(str(i) for i in digits))
+        if num == 0:
+            digits[-1] = 1
+            return digits
+        else:
+            return str(num+1)
+        
+        # <Solution 2>
+        return str(int("".join(str(i) for i in digits))+1).zfill(len(digits))
