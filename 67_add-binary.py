28bbbef4d88b11c7f808f3a8e4b85838ebb4ecfa
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2021-01-05 00:59:27 +0800

    Create 67_add-binary.py

---
+++ 67_add-binary.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        # <Solution 1>
+        return bin(int(a, 2) + int(b, 2))[2:]
+        
+        # <Solution 2>
+        a_len = len(a)
+        b_len = len(b)
+        carry = 0
+        bin_sum = ""
+        # Sum corresponding digits in two numbers
+        while a_len > 0 or b_len > 0:
+            # digit_sum is set as the carry from previous round
+            digit_sum = carry
+            if b_len > 0:
+                digit_sum += int(b[b_len-1])
+                b_len -= 1
+            if a_len > 0:
+                digit_sum += int(a[a_len-1])
+                a_len -= 1
+            bin_sum = ''.join((str(digit_sum % 2), bin_sum))
+            carry = digit_sum / 2
+        if carry != 0:
+            bin_sum = ''.join((str(carry), bin_sum))
+        return bin_sum
