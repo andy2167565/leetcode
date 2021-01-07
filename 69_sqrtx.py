7065e5be7a8636941f433a7cbaaed33a1bb37c8e
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2021-01-08 00:44:00 +0800

    Create 69_sqrtx.py

---
+++ 69_sqrtx.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        # <Solution 1>
+        return int(sqrt(x))
+        
+        # <Solution 2>: Binary Search
+        low = 1
+        high = x
+        while low <= high:
+            mid = (low+high) // 2
+            if mid**2 == x:
+                return mid
+            elif mid**2 > x:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return high
