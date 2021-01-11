3938d4a08f610c74a2938a5d296266653b83f5bb
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2021-01-12 02:44:18 +0800

    Create 70_climbing-stairs.py

---
+++ 70_climbing-stairs.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # <Solution 1>: Fibonacci Sequence - Bottom-up
+        if n < 3:
+            return n
+        a, b = 1, 2
+        while n > 2:        
+            a, b = b, a+b
+            n -= 1
+        return b
+        
+        # <Solution 2>: Fibonacci Formula
+        return int((pow((1+sqrt(5))/2, n+1)-pow((1-sqrt(5))/2, n+1))/sqrt(5))
+        
+        # <Solution 3>: Dynamic Programming
+        dp = [1, 2]
+        for i in range(2, n):
+            dp.append(dp[i-1]+dp[i-2])
+        return dp[n-1]
+        
+        # <Solution 4>: Combinations with Replacement
+        from math import factorial
+        ones = n % 2
+        twos = n // 2
+        result = 0
+        while twos >= 0:
+            result += factorial(ones + twos) // (factorial(ones) * factorial(twos))
+            twos -= 1
+            ones += 2
+        return result
