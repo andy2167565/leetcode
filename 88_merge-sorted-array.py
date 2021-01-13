87f0eb18f5eb3fc0afef35a6226f2c40bb845e58
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2021-01-13 18:55:59 +0800

    Create 88_merge-sorted-array.py

---
+++ 88_merge-sorted-array.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        # <Solution 1>
+        while nums2:
+            nums1.append(nums2[-1])
+            nums1.remove(0)
+            del nums2[-1]
+        nums1.sort()
+        
+        # <Solution 2>
+        nums1[m:] = nums2
+        nums1.sort()
+        
+        # <Solution 3>
+        i = m + n - 1
+        m -= 1
+        n -= 1
+        # Scan both lists from the end and take larger one
+        while m >= 0 and n >= 0:
+            if nums1[m] > nums2[n]:
+                nums1[i] = nums1[m]
+                m -= 1
+            else:
+                nums1[i] = nums2[n]
+                n -= 1
+            i -= 1
+        # All elements in nums1 have been sorted and placed from the end
+        # Put remaining elements in nums2 into the head of nums1
+        if m < 0:
+            nums1[:i+1] = nums2[:n+1]
