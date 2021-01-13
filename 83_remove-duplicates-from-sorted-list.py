0c9dd8a4bdd07129d452bf171a7fb566ef93e2d7
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2021-01-13 16:15:49 +0800

    Create 83_remove-duplicates-from-sorted-list.py

---
+++ 83_remove-duplicates-from-sorted-list.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        # <Iterative Solution>
+        current = head
+        # Loop through each type of digits -> 1, 2, 3, etc.
+        while current:
+            # Search identical digits
+            while current.next and current.val == current.next.val:
+                current.next = current.next.next
+            current = current.next
+        return head
+        
+        # <Recursive Solution>
+        if head and head.next:
+            if head.val == head.next.val:
+                # Remove head.next and delete duplicates for head again
+                head.next = head.next.next
+                head = self.deleteDuplicates(head)
+            else:
+                # Delete duplicates for head.next
+                head.next = self.deleteDuplicates(head.next)
+        return head
