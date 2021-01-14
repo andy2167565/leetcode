30ab2e74a0fc16999c4318bdd16f3c4b83a282b9
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2021-01-14 23:31:39 +0800

    Update 66_plus-one.py

--- Easy/66_plus-one.py
+++ Easy/66_plus-one.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        # <Solution 1>
+#======== <Solution 1> ========#
         num = int("".join(str(i) for i in digits))
         if num == 0:
             digits[-1] = 1
@@ -12,5 +12,5 @@ class Solution(object):
         else:
             return str(num+1)
         
-        # <Solution 2>
+#======== <Solution 2> ========#
         return str(int("".join(str(i) for i in digits))+1).zfill(len(digits))
