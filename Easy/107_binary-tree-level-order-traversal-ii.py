ae9418591cbe5ad0eee96dd97df1107a1e53d930
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2021-01-18 00:25:20 +0800

    Create 107_binary-tree-level-order-traversal-ii.py

---
+++ Easy/107_binary-tree-level-order-traversal-ii.py
@@ -0,0 +1,55 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+#======== <Iterative Solution 1>: Use List as Queue ========#
+        if not root:
+            return []
+        result = []
+        cur_level = []
+        # Use None to separate each level
+        queue = [root, None]
+        while queue:
+            node = queue.pop(0)
+            if node:
+                # Collect node values in current level
+                cur_level.append(node.val)
+                # Append nodes in next level if exist
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            else:
+                if cur_level:
+                    # Add node values in current level to result list
+                    result.insert(0, cur_level)
+                    cur_level = []
+                    # Use None to separate each level
+                    queue.append(None)
+        return result
+        
+#======== <Iterative Solution 2> ========#
+        if not root:
+            return []
+        result = []
+        cur_level = [root]
+        while cur_level:
+            # Add node values in current level to result list
+            result.insert(0, [node.val for node in cur_level])
+            # Generate node list of next level
+            next_level = []
+            for node in cur_level:
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+            cur_level = next_level
+        return result
