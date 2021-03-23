522472a02f6393196eb88a2f5bd8b8b543e9703f
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2021-03-23 12:50:35 +0800

    Create same-tree.py

---
+++ Easy/0100_same-tree/same-tree.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+#======== <Recursive Solution 1> ========#
+        if not p and not q:
+            return True
+        elif p and q:
+            same_val = p.val == q.val
+            same_left = self.isSameTree(p.left, q.left)
+            same_right = self.isSameTree(p.right, q.right)
+            return same_val and same_left and same_right
+        else:
+            return False
+        
+#======== <Recursive Solution 2> ========#
+        if not p or not q:
+            return p == q
+        return (p.val == q.val) and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+#======== <Iterative Solution> ========#
+        stack = [(p, q)]
+        while stack:
+            # Pop last pair from the stack
+            l, r = stack.pop()
+            if l and r:
+                if l.val != r.val:
+                    return False
+                else:
+                    stack.append((l.left, r.left))
+                    stack.append((l.right, r.right))
+            elif l or r:
+                return False
+        return True
