6593c68c7d173d91ad351de7f55e7698086d1b32
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-08-10 10:55:35 +0800

    Update reverse-integer.py
    Changed to Python3 format

--- Medium/0007_reverse-integer/reverse-integer.py
+++ Medium/0007_reverse-integer/reverse-integer.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def reverse(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
+class Solution:
+    def reverse(self, x: int) -> int:
         reversed_num = int(''.join([i for i in reversed(list(str(abs(x))))]))
         reversed_num = -reversed_num if x < 0 else reversed_num
         return reversed_num if -pow(2, 31) <= reversed_num <= pow(2, 31)-1 else 0
