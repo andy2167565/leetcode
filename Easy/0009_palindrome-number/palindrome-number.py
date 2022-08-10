9ade409dcc27840806172b32d8dc0bc63a5d76f0
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-08-10 10:57:02 +0800

    Update palindrome-number.py
    Changed to Python3 format

--- Easy/0009_palindrome-number/palindrome-number.py
+++ Easy/0009_palindrome-number/palindrome-number.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
         if x < 0 or (x % 10 == 0 and x != 0):
             return False
         
