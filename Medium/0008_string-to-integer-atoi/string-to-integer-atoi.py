51be33b8b60073329c62fc76d7a3eef68471b460
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-08-10 15:54:48 +0800

    Create string-to-integer-atoi.py

---
+++ Medium/0008_string-to-integer-atoi/string-to-integer-atoi.py
@@ -0,0 +1,17 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+#======== <Solution 1> ========#
+        s = s.lstrip()
+        sign = 1
+        if s and s[0] in '+-':
+            if s[0] == '-': sign = -1
+            s = s[1:]
+        num = 0
+        for c in s:
+            if not c.isdigit(): break
+            num = 10 * num + int(c)
+        return max(-2**31, min(2**31 - 1, sign * num))
+
+#======== <Solution 2> ========#
+        num = re.search(r"^[-+]?\d+", s.lstrip())
+        return max(min(int(num.group(0)), 2**31-1), -2**31) if num else 0
