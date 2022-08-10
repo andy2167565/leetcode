49acfc5513bcc7c99b60b63067e7c0d646d8632a
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-08-10 12:25:20 +0800

    Update pascals-triangle.py
    Changed to Python3 format

--- Easy/0118_pascals-triangle/pascals-triangle.py
+++ Easy/0118_pascals-triangle/pascals-triangle.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def generate(self, numRows):
-        """
-        :type numRows: int
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
 #======== <Solution 1> ========#
         result = [[1]]
         if numRows == 1:
