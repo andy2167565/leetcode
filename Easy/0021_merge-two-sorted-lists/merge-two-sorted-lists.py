ae73a7e1f3905a9a07cce2374a6d4b3fc50521da
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-08-10 11:11:40 +0800

    Update merge-two-sorted-lists.py
    Changed to Python3 format

--- Easy/0021_merge-two-sorted-lists/merge-two-sorted-lists.py
+++ Easy/0021_merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -1,45 +1,40 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
 #======== <Recursive Solution> ========#
-        if not l1:
-            return l2
-        if not l2:
-            return l1
+        if not list1:
+            return list2
+        if not list2:
+            return list1
         
-        if l1.val < l2.val:
-            l1.next = self.mergeTwoLists(l1.next, l2)
-            return l1
+        if list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
         else:
-            l2.next = self.mergeTwoLists(l2.next, l1)
-            return l2
+            list2.next = self.mergeTwoLists(list2.next, list1)
+            return list2
         
 #======== <Iterative Solution> ========#
         # Ref: https://stackoverflow.com/questions/56515975/python-logic-of-listnode-in-leetcode
         result = ListNode(0)
         result_tail = result
         
-        while l1 and l2:
-            if l1.val < l2.val:
-                result_tail.next = l1
-                l1 = l1.next
+        while list1 and list2:
+            if list1.val < list2.val:
+                result_tail.next = list1
+                list1 = list1.next
             else:
-                result_tail.next = l2
-                l2 = l2.next
+                result_tail.next = list2
+                list2 = list2.next
             result_tail = result_tail.next
         
-        if l1:
-            result_tail.next = l1
-        elif l2:
-            result_tail.next = l2
+        if list1:
+            result_tail.next = list1
+        elif list2:
+            result_tail.next = list2
         
         return result.next
