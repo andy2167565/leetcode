f1beae1ffd046f46cfb2fdf6e94698bc62cfe90c
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-08-10 12:19:29 +0800

    Update balanced-binary-tree.py
    Changed to Python3 format

--- Easy/0110_balanced-binary-tree/balanced-binary-tree.py
+++ Easy/0110_balanced-binary-tree/balanced-binary-tree.py
@@ -1,15 +1,11 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def isBalanced(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
 #======== <Recursive Solution 1> ========#
         height, result = self.getResult(root)
         return result
