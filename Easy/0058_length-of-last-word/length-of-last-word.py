da5b5d7c6488445ad293a490027cad63417f96bc
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-08-10 11:23:29 +0800

    Update length-of-last-word.py
    Changed to Python3 format

--- Easy/0058_length-of-last-word/length-of-last-word.py
+++ Easy/0058_length-of-last-word/length-of-last-word.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def lengthOfLastWord(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
         try:
             return len(s.split()[-1])
         except:
