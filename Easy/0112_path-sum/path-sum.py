c4619aa34b39cfcc6cec0ca9f86501bb94a7e13f
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-08-10 12:23:36 +0800

    Update path-sum.py
    Changed to Python3 format

--- Easy/0112_path-sum/path-sum.py
+++ Easy/0112_path-sum/path-sum.py
@@ -1,16 +1,11 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def hasPathSum(self, root, targetSum):
-        """
-        :type root: TreeNode
-        :type targetSum: int
-        :rtype: bool
-        """
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
 #======== <Recursive Solution 1> ========#
         if not root:
             return False
