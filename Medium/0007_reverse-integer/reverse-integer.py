cd0c4651f5dcbb485d26966a2d1487703c078692
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-08-10 14:12:36 +0800

    Update reverse-integer.py
    Added new solution

--- Medium/0007_reverse-integer/reverse-integer.py
+++ Medium/0007_reverse-integer/reverse-integer.py
@@ -1,5 +1,18 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        reversed_num = int(''.join([i for i in reversed(list(str(abs(x))))]))
+#======== <Solution 1> ========#
+        reversed_num = int(''.join(reversed(list(str(abs(x))))))
         reversed_num = -reversed_num if x < 0 else reversed_num
         return reversed_num if -pow(2, 31) <= reversed_num <= pow(2, 31)-1 else 0
+
+#======== <Solution 2> ========#
+        if x < 0:
+            return -self.reverse(-x)
+        elif x == 0:
+            return 0
+        else:
+            ans = 0
+            while x > 0:
+                x, r = divmod(x, 10)
+                ans = 10 * ans + r
+            return ans if ans <= 2**31 - 1 else 0
