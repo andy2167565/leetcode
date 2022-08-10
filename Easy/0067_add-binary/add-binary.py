24045a3a8041eb61d84477dd457f01a9d91a4080
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-08-10 11:56:06 +0800

    Update add-binary.py
    Changed to Python3 format

--- Easy/0067_add-binary/add-binary.py
+++ Easy/0067_add-binary/add-binary.py
@@ -1,10 +1,5 @@
-class Solution(object):
-    def addBinary(self, a, b):
-        """
-        :type a: str
-        :type b: str
-        :rtype: str
-        """
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
 #======== <Solution 1> ========#
         return bin(int(a, 2) + int(b, 2))[2:]
         
@@ -24,7 +19,7 @@ class Solution(object):
                 digit_sum += int(a[a_len-1])
                 a_len -= 1
             bin_sum = ''.join((str(digit_sum % 2), bin_sum))
-            carry = digit_sum / 2
+            carry = int(digit_sum / 2)
         if carry != 0:
             bin_sum = ''.join((str(carry), bin_sum))
         return bin_sum
