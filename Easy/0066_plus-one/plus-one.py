5417e34ebf6691649b491b7d7f7dc21974c190a4
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-08-10 17:15:47 +0800

    Update plus-one.py
    Added new solution

--- Easy/0066_plus-one/plus-one.py
+++ Easy/0066_plus-one/plus-one.py
@@ -1,12 +1,16 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
 #======== <Solution 1> ========#
-        num = int("".join(str(i) for i in digits))
-        if num == 0:
-            digits[-1] = 1
-            return digits
-        else:
-            return str(num+1)
-        
+        return [int(n) for n in str(int("".join(str(i) for i in digits))+1)]
+
 #======== <Solution 2> ========#
-        return str(int("".join(str(i) for i in digits))+1).zfill(len(digits))
+        return list(map(int, str(int("".join(map(str, digits)))+1)))
+
+#======== <Solution 3> ========#
+        if digits[-1] == 9:
+            if len(digits) == 1:
+                return [1, 0]
+            return self.plusOne(digits[:-1]) + [0]
+        else:
+            digits[-1] += 1
+        return digits
