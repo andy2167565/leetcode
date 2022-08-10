9a0a07db76e7f6220ce59f77583241a3990b0fd3
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-08-10 12:09:58 +0800

    Update same-tree.py
    Changed to Python3 format

--- Easy/0100_same-tree/same-tree.py
+++ Easy/0100_same-tree/same-tree.py
@@ -1,16 +1,11 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def isSameTree(self, p, q):
-        """
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: bool
-        """
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
 #======== <Recursive Solution 1> ========#
         if not p and not q:
             return True
