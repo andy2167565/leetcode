e827aa032eb4cd1a0ca8e328b883ef681464bfde
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-08-14 15:50:23 +0800

    Update power-of-two.py
    Added new solutions

--- Easy/0231_power-of-two/power-of-two.py
+++ Easy/0231_power-of-two/power-of-two.py
@@ -1,10 +1,25 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
 #======== <Solution 1> ========#
-        return n > 0 and n & (n - 1) == 0
+        return n > 0 and 2**30 % n == 0
 
 #======== <Solution 2> ========#
-        return n > 0 and sum(list(map(int, bin(n)[2:]))) == 1
+        import math
+        return n > 0 and (math.log10(n) / math.log10(2)) % 1 == 0
 
 #======== <Solution 3> ========#
-        return n > 0 and 2 ** 30 % n == 0
+        if n < 1: return False
+        while n % 2 == 0:
+            n //= 2
+        return n == 1
+
+#======== <Solution 4> ========#
+        if n < 1: return False
+        if n == 1: return True
+        return n % 2 == 0 and self.isPowerOfTwo(n // 2)
+
+#======== <Solution 5> ========#
+        return n > 0 and n & (n - 1) == 0
+
+#======== <Solution 6> ========#
+        return n > 0 and sum(list(map(int, bin(n)[2:]))) == 1
