09be7194753a962ebd8a285a0e14b987d1593547
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-08-14 15:32:23 +0800

    Create power-of-four.py

---
+++ Easy/0342_power-of-four/power-of-four.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+#======== <Solution 1> ========#
+        import math
+        return n > 0 and (math.log10(n) / math.log10(4)) % 1 == 0
+
+#======== <Solution 2> ========#
+        import math
+        return n > 0 and math.log(n, 4).is_integer()
+
+#======== <Solution 3> ========#
+        if n < 1: return False
+        while n % 4 == 0:
+            n //= 4
+        return n == 1
+
+#======== <Solution 4> ========#
+        if n < 1: return False
+        if n == 1: return True
+        return n % 4 == 0 and self.isPowerOfFour(n // 4)
+
+#======== <Solution 5> ========#
+        # Reference: https://ithelp.ithome.com.tw/articles/10240132
+        return n & (n - 1) == 0 and n & 0x55555555 != 0
+
+#======== <Solution 6> ========#
+        return n > 0 and n & (n - 1) == 0 and len(bin(n)) % 2 == 1
+
+#======== <Solution 7> ========#
+        return n & (n - 1) == 0 and (n % 3) & 1 == 1
