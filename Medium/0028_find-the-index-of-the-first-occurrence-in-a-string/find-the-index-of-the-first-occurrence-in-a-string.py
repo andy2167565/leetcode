1cd45fb0abbe62dbe285167c87cffba9c7df442e
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-09-05 12:15:10 +0800

    Update find-the-index-of-the-first-occurrence-in-a-string.py
    Updated existing solutions and added new ones

--- Medium/0028_find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ Medium/0028_find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,29 +1,21 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
 #======== <Solution 1> ========#
-        if not needle:
-            return 0
-        
         hs_id = 0
-        hs_list = list(haystack)
-        nd_list = list(needle)
-        while hs_id < len(hs_list):
-            try:
-                if hs_list[hs_id:hs_id+len(nd_list)] != nd_list:
-                    raise Exception
+        while hs_id < len(haystack):
+            if haystack[hs_id: hs_id + len(needle)] == needle:
                 return hs_id
-            except Exception:
-                hs_id += 1
+            hs_id += 1
         return -1
-        
+
 #======== <Solution 2> ========#
         for hs_id in range(len(haystack) - len(needle) + 1):
-            if all(haystack[hs_id+nd_id] == needle[nd_id] for nd_id in range(len(needle))):
+            if all(haystack[hs_id + nd_id] == needle[nd_id] for nd_id in range(len(needle))):
                 return hs_id
         return -1
         
 #======== <Solution 3> ========#
-        try:
-            return haystack.index(needle)
-        except:
-            return -1
+        return haystack.index(needle) if needle in haystack else -1
+
+#======== <Solution 4> ========#
+        return haystack.find(needle)
