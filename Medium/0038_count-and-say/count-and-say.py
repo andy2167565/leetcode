c2810cdec78d417bee4f121c18ae082fa9aa286b
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-09-24 15:35:44 +0800

    Update count-and-say.py
    Updated existing solutions and added new ones

--- Medium/0038_count-and-say/count-and-say.py
+++ Medium/0038_count-and-say/count-and-say.py
@@ -1,18 +1,17 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
-#======== <Iterative Solution> ========#
-        result = "1"
-        # Loop n-1 times since the string for n=1 is already defined
+#======== <Solution 1> ========#
+        result = '1'
+        # Loop n - 1 times since the string for n = 1 is already defined
         while n > 1:
             # Pointer for characters
-            char = 0
-            current = ""
+            char, current = 0, ''
             # Loop for each group in the string
             while char < len(result):
                 # Count identical characters in each group
                 count = 1
-                # char+1 < len(result): The character next to char is in the range of string
-                while char+1 < len(result) and result[char] == result[char+1]:
+                # char + 1 < len(result): The character next to char is in the range of string
+                while char + 1 < len(result) and result[char] == result[char + 1]:
                     count += 1
                     char += 1
                 current += str(count) + result[char]
@@ -20,20 +19,52 @@ class Solution:
             result = current
             n -= 1
         return result
+
+#======== <Solution 2> ========#
+        result = '1'
+        for _ in range(n - 1):
+            letter, current, count = result[0], '', 0
+            for l in result:
+                if letter == l:
+                    count += 1
+                else:
+                    current += str(count) + letter
+                    letter = l
+                    count = 1
+            current += str(count) + letter
+            result = current
+        return result
         
-#======== <Recursive Solution> ========#
-        if n == 1:
-            return "1"
-        
-        seq = self.countAndSay(n-1)
-        
-        char = 0
-        result = ""
-        while char < len(seq):
+#======== <Solution 3> ========#
+        if n == 1: return '1'
+        current = self.countAndSay(n - 1)
+        char, result = 0, ''
+        while char < len(current):
             count = 1
-            while char+1 < len(seq) and seq[char] == seq[char+1]:
+            while char + 1 < len(current) and current[char] == current[char + 1]:
                 count += 1
                 char += 1
-            result += str(count) + seq[char]
+            result += str(count) + current[char]
             char += 1
+        return result
+
+#======== <Solution 4> ========#
+        import itertools
+        result = '1'
+        for _ in range(n - 1):
+            result = ''.join(str(sum(map(bool, v))) + k for k, v in itertools.groupby(result))
+        return result
+
+#======== <Solution 5> ========#
+        import re
+        result = '1'
+        for _ in range(n - 1):
+            result = re.sub(r'(.)\1*', lambda m: str(len(m.group(0))) + m.group(1), result)
+        return result
+
+#======== <Solution 6> ========#
+        import re
+        result = '1'
+        for _ in range(n - 1):
+            result = ''.join(str(len(group)) + digit for group, digit in re.findall(r'((.)\2*)', result))
         return result
