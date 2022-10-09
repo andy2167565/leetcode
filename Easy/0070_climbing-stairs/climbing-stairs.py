a212c48e11c2e4ea3fd56f109dca5b991c74a5b6
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-10-09 12:19:56 +0800

    Update climbing-stairs.py
    Updated existing solutions and added new ones

--- Easy/0070_climbing-stairs/climbing-stairs.py
+++ Easy/0070_climbing-stairs/climbing-stairs.py
@@ -1,30 +1,38 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-#======== <Solution 1>: Fibonacci Sequence - Bottom-up ========#
-        if n < 3:
-            return n
-        a, b = 1, 2
-        while n > 2:        
-            a, b = b, a+b
-            n -= 1
-        return b
-        
-#======== <Solution 2>: Fibonacci Formula ========#
-        return int((pow((1+sqrt(5))/2, n+1)-pow((1-sqrt(5))/2, n+1))/sqrt(5))
-        
-#======== <Solution 3>: Dynamic Programming ========#
-        dp = [1, 2]
-        for i in range(2, n):
-            dp.append(dp[i-1]+dp[i-2])
-        return dp[n-1]
-        
-#======== <Solution 4>: Combinations with Replacement ========#
-        from math import factorial
-        ones = n % 2
-        twos = n // 2
-        result = 0
+#======== <Solution 1>: Combinations with Replacement ========#
+        import math
+        twos, ones = divmod(n, 2)
+        ans = 0
         while twos >= 0:
-            result += factorial(ones + twos) // (factorial(ones) * factorial(twos))
+            ans += math.factorial(ones + twos) // (math.factorial(ones) * math.factorial(twos))
             twos -= 1
             ones += 2
-        return result
+        return ans
+
+#======== <Solution 2>: Fibonacci Sequence - Bottom-up ========#
+        if n < 3: return n
+        prev, curr = 1, 2
+        for _ in range(2, n):
+            prev, curr = curr, prev + curr
+        return curr
+
+#======== <Solution 3>: Fibonacci Formula ========#
+        # Reference: https://en.wikipedia.org/wiki/Fibonacci_number#Closed-form_expression
+        import math
+        return int((pow((1 + math.sqrt(5)) / 2, n + 1) - pow((1 - math.sqrt(5)) / 2, n + 1)) / math.sqrt(5))
+
+# Reference: https://leetcode.com/problems/climbing-stairs/discuss/1792723/Python-or-In-Depth-Walkthrough-%2B-Explanation-or-DP-Top-Down-%2B-Bottom-Up
+#======== <Solution 4>: Dynamic Programming - Top-down with Memoization ========#
+        def helper(n: int) -> int:
+            if n not in memo:
+                memo[n] = helper(n - 1) + helper(n - 2)
+            return memo[n]
+        memo = {1: 1, 2: 2}
+        return helper(n)
+
+#======== <Solution 5>: Dynamic Programming - Bottom-up ========#
+        dp = [1, 2]
+        for i in range(2, n):
+            dp.append(dp[i - 1] + dp[i - 2])
+        return dp[n - 1]
