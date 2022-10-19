e0d9a218b10901a9319b182021c2d1bd8e9472c3
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-10-19 17:03:54 +0800

    Update search-insert-position.py
    Updated existing solutions and added new ones

--- Easy/0035_search-insert-position/search-insert-position.py
+++ Easy/0035_search-insert-position/search-insert-position.py
@@ -1,8 +1,45 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        try:
-            return nums.index(target)
-        except:
+#======== <Solution 1> ========#
+        for i, num in enumerate(nums):
+            if num >= target:
+                return i
+        return len(nums)
+
+#======== <Solution 2> ========#
+        return sum(num < target for num in nums)
+
+#======== <Solution 3> ========#
+        if target not in nums:
             nums.append(target)
             nums.sort()
-            return nums.index(target)
+        return nums.index(target)
+
+# Reference: https://leetcode.com/problems/search-insert-position/discuss/423166/Binary-Search-101
+#======== <Solution 4> ========#
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) >> 1
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return l
+
+#======== <Solution 5> ========#
+        # Reference: https://github.com/python/cpython/blob/3.10/Lib/bisect.py
+        # bisect_left function in bisect module
+        lo, hi = 0, len(nums)
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if nums[mid] < target:
+                lo = mid + 1
+            else:
+                hi = mid
+        return lo
+
+#======== <Solution 6> ========#
+        import bisect
+        return bisect.bisect_left(nums, target)
