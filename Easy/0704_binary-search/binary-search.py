1e2b3bd364818c4cf26f23501db0bfb1f2cca760
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-10-19 17:29:32 +0800

    Update binary-search.py
    Updated existing solutions and added new ones

--- Easy/0704_binary-search/binary-search.py
+++ Easy/0704_binary-search/binary-search.py
@@ -11,9 +11,9 @@ class Solution:
             return nums.index(target)
         return -1
 
+# Reference: https://leetcode.com/problems/binary-search/discuss/423162/Binary-Search-101
 #======== <Solution 3> ========#
-        # Reference 1 :https://leetcode.com/problems/binary-search/discuss/423162/Binary-Search-101
-        # Reference 2: https://leetcode.com/problems/binary-search/discuss/1914265/Binary-Search-for-beginners-(Intuition-building)
+        # Reference: https://leetcode.com/problems/binary-search/discuss/1914265/Binary-Search-for-beginners-(Intuition-building)
         l, r = 0, len(nums) - 1
         while l <= r:
             mid = (l + r) >> 1
@@ -24,3 +24,20 @@ class Solution:
             else:
                 r = mid - 1
         return -1
+
+#======== <Solution 4> ========#
+        # Reference: https://github.com/python/cpython/blob/3.10/Lib/bisect.py
+        # A variation of bisect_left function in bisect module
+        lo, hi = 0, len(nums) - 1  # hi = len(nums) - 1 since we won't insert target into nums. No need for a spare space to insert the number
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if nums[mid] < target:
+                lo = mid + 1
+            else:
+                hi = mid
+        return lo if nums[lo] == target else -1
+
+#======== <Solution 5> ========#
+        import bisect
+        index = bisect.bisect_left(nums, target)
+        return index if index < len(nums) and nums[index] == target else -1
