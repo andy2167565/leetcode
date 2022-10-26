ef5776ff036e8de1be6a7955834202fb4eab821b
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-10-26 14:08:33 +0800

    Update maximum-depth-of-binary-tree.py
    Updated existing solutions

--- Easy/0104_maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ Easy/0104_maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -6,24 +6,18 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-#======== <Recursive Solution> ========#
-        if not root:
-            return 0
-        else:
-            return max(self.maxDepth(root.left)+1, self.maxDepth(root.right)+1)
-        
-#======== <Iterative Solution> ========#
-        if not root:
-            return 0
-        stack = [(root, 1)]
-        max_level = 0
+#======== <Solution 1> ========#
+        if not root: return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+#======== <Solution 2> ========#
+        if not root: return 0
+        stack, max_level = [(root, 1)], 0
         while stack:
-            # Store the level for each node
-            node, level = stack.pop()
+            node, level = stack.pop()  # Store the level for each node
             if node.left:
-                stack.append((node.left, level+1))
+                stack.append((node.left, level + 1))
             if node.right:
-                stack.append((node.right, level+1))
-            # Update maximum level in case it start counting nodes from lower level
-            max_level = max(level, max_level)
+                stack.append((node.right, level + 1))
+            max_level = max(level, max_level)  # Update maximum level in case it start counting nodes from lower level
         return max_level
