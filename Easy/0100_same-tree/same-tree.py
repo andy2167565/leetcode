23bf375573045229dc95084a847f9b167c278392
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-10-27 12:36:05 +0800

    Update same-tree.py

--- Easy/0100_same-tree/same-tree.py
+++ Easy/0100_same-tree/same-tree.py
@@ -6,33 +6,21 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-#======== <Recursive Solution 1> ========#
-        if not p and not q:
-            return True
-        elif p and q:
-            same_val = p.val == q.val
-            same_left = self.isSameTree(p.left, q.left)
-            same_right = self.isSameTree(p.right, q.right)
-            return same_val and same_left and same_right
-        else:
-            return False
-        
-#======== <Recursive Solution 2> ========#
+#======== <Solution 1> ========#
         if not p or not q:
             return p == q
-        return (p.val == q.val) and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        
-#======== <Iterative Solution> ========#
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+#======== <Solution 2> ========#
         stack = [(p, q)]
         while stack:
-            # Pop last pair from the stack
             l, r = stack.pop()
-            if l and r:
-                if l.val != r.val:
-                    return False
-                else:
-                    stack.append((l.left, r.left))
-                    stack.append((l.right, r.right))
-            elif l or r:
+            if l and r and l.val == r.val:
+                stack.extend([(l.left, r.left), (l.right, r.right)])
+            # Possible cases:
+            # 1) l and r and l.val != r.val
+            # 2) l or r
+            # l == r only when both l and r are None
+            elif l != r:
                 return False
         return True
