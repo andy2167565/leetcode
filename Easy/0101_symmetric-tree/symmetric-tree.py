34ad04b828ca07239c12b16fbf06c33dd76348dc
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-10-28 16:11:16 +0800

    Update symmetric-tree.py
    Updated existing solutions

--- Easy/0101_symmetric-tree/symmetric-tree.py
+++ Easy/0101_symmetric-tree/symmetric-tree.py
@@ -6,13 +6,9 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-#======== <Recursive Solution 1> ========#
-        if not root:
-            return True
-        # Mirror one of the child
-        self.mirror(root.left)
-        # Compare two children to see if they are the same
-        return self.isSameTree(root.left, root.right)
+#======== <Solution 1> ========#
+        self.mirror(root.left)  # Mirror one of the child
+        return self.isSameTree(root.left, root.right)  # Compare two children to see if they are the same
 
     def mirror(self, node):
         if node:
@@ -23,32 +19,22 @@ class Solution:
     def isSameTree(self, p, q):
         if not p or not q:
             return p == q
-        return (p.val == q.val) and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        
-#======== <Recursive Solution 2> ========#
-        if not root:
-            return True
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+#======== <Solution 2> ========#
         return self.isMirror(root.left, root.right)
-        
+
     def isMirror(self, node1, node2):
-        if not node1 or not node2:
-            return node1 == node2
-        if node1.val != node2.val:
-            return False
-        return self.isMirror(node1.left, node2.right) and self.isMirror(node1.right, node2.left)
-        
-#======== <Iterative Solution> ========#
-        if not root:
-            return True
+        if node1 and node2 and node1.val == node2.val:
+            return self.isMirror(node1.left, node2.right) and self.isMirror(node1.right, node2.left)
+        return node1 == node2
+
+#======== <Solution 3> ========#
         stack = [(root.left, root.right)]
         while stack:
             l, r = stack.pop()
-            if l and r:
-                if l.val != r.val:
-                    return False
-                else:
-                    stack.append((l.left, r.right))
-                    stack.append((l.right, r.left))
+            if l and r and l.val == r.val:
+                stack.extend([(l.left, r.right), (l.right, r.left)])
             elif l or r:
                 return False
         return True
