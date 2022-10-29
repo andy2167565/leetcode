734eac01910663e6c27e3d7aa08687d3b99345a8
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2022-10-29 12:21:37 +0800

    Update palindrome-number.py
    Updated existing solutions and added new ones

--- Easy/0009_palindrome-number/palindrome-number.py
+++ Easy/0009_palindrome-number/palindrome-number.py
@@ -1,12 +1,14 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0 or (x % 10 == 0 and x != 0):
-            return False
-        
-        reversed_num = 0
-        while x > reversed_num:
-            r = x % 10
-            reversed_num = reversed_num * 10 + r 
-            x //= 10
+#======== <Solution 1> ========#
+        return str(x) == str(x)[::-1]
 
-        return x == reversed_num or x == reversed_num//10
+#======== <Solution 2> ========#
+        if x < 0 or (x and not x % 10): return False
+        if x < 10: return True
+        reversed_num = 0
+        # Reverse the last half of the integer
+        while x > reversed_num:
+            x, r = divmod(x, 10)
+            reversed_num = reversed_num * 10 + r
+        return x == reversed_num or x == reversed_num // 10  # Even length or odd length integer
