257ae424d8538b161c603b1b25599b135bb898b1
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2023-11-24 11:09:50 +0800

    Create insufficient-nodes-in-root-to-leaf-paths.py

---
+++ Medium/1080_insufficient-nodes-in-root-to-leaf-paths/insufficient-nodes-in-root-to-leaf-paths.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sufficientSubset(self, root: Optional[TreeNode], limit: int) -> Optional[TreeNode]:
+        # Reference: https://leetcode.com/problems/insufficient-nodes-in-root-to-leaf-paths/solutions/308326/java-c-python-easy-and-concise-recursion/
+        if root.left == root.right:  # root is leaf
+            return None if root.val < limit else root
+        if root.left:
+            root.left = self.sufficientSubset(root.left, limit - root.val)
+        if root.right:
+            root.right = self.sufficientSubset(root.right, limit - root.val)
+        return root if root.left or root.right else None
