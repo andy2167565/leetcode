7446ab8ae1ba0e52b10ba24c3b08a428e11ab449
Andy Lin <50549530+andy2167565@users.noreply.github.com>
2023-12-31 12:00:43 +0800

    Create stock-price-fluctuation.py

---
+++ Medium/2034_stock-price-fluctuation/stock-price-fluctuation.py
@@ -0,0 +1,34 @@
+import sortedcontainers
+class StockPrice:
+
+    def __init__(self):
+        self.time_to_price = sortedcontainers.SortedDict()
+        self.price_to_time = sortedcontainers.SortedDict()
+
+    def update(self, timestamp: int, price: int) -> None:
+        if timestamp in self.time_to_price:
+            prev_price = self.time_to_price[timestamp]
+            self.price_to_time[prev_price].remove(timestamp)
+            if not self.price_to_time[prev_price]:
+                self.price_to_time.pop(prev_price)
+        if price not in self.price_to_time:
+            self.price_to_time[price] = set()
+        self.price_to_time[price].add(timestamp)
+        self.time_to_price[timestamp] = price
+
+    def current(self) -> int:
+        return self.time_to_price.peekitem(-1)[1]
+
+    def maximum(self) -> int:
+        return self.price_to_time.peekitem(-1)[0]
+
+    def minimum(self) -> int:
+        return self.price_to_time.peekitem(0)[0]
+
+
+# Your StockPrice object will be instantiated and called as such:
+# obj = StockPrice()
+# obj.update(timestamp,price)
+# param_2 = obj.current()
+# param_3 = obj.maximum()
+# param_4 = obj.minimum()
